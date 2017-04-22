f3951904e3fbb4d8288077523ab499ed84e45a1c
cubancigaro <Maikel.correa@icloud.com>
2017-04-22 09:40:21 -0500

    more color

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
-//  $("td:contains('Red')").css("background-color", "red");
 
-//  $("td:contains('Blue')").css("background-color", "blue");
-  
-//  $("td:contains('Green')").css("background-color", "green");
-  
-//  $("td:contains('Yellow')").css("background-color", "yellow");
-  
-//  $("td:contains('Yellow')").css("color", "black");
-  
-//  $("td:contains('Orange')").css("background-color", "orange");
-  
-//  $("td:contains('Pink')").css("background-color", "pink");
-  
-//  $("td:contains('Pink')").css("color", "black");
+$("td:contains('Red')").css("background-color", "red");
+
+$("td:contains('Blue')").css("background-color", "blue");
+
+$("td:contains('Green')").css("background-color", "green");
+
+$("td:contains('Yellow')").css("background-color", "yellow");
+
+$("td:contains('Yellow')").css("color", "black");
+
+$("td:contains('Orange')").css("background-color", "orange");
+
+$("td:contains('Pink')").css("background-color", "pink");
+
+$("td:contains('Pink')").css("color", "black");
 });
